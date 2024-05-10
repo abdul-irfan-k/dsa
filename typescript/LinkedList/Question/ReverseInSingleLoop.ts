800e67563c2618d3d21eeba328174627fe7c2ae0
Irfan K <unknown7k7k@gmail.com>
2024-05-10 12:22:07 +0530

    Solved reverse singly linked list in single loop

---
+++ typescript/LinkedList/Question/ReverseInSingleLoop.ts
@@ -0,0 +1,41 @@
+import { SingleLinkedList, Node } from "../SinglyLinkedList";
+
+type NodeType<T> = Node<T> | null;
+class SlinkedList<T> extends SingleLinkedList<T> {
+  reverseInSingleLoop() {
+    let temp = this.head;
+
+    if (!temp || !temp.next || !temp.next.next) return;
+
+    let prev: NodeType<T> = temp?.next;
+    let current: NodeType<T> = temp;
+    let prevOldNode: NodeType<T> = temp;
+
+    temp = temp.next;
+    while (temp != null) {
+      current.next = temp.next;
+      prev = temp;
+      prev.next = prevOldNode;
+      prevOldNode = prev;
+
+      temp = current.next;
+      if (current.next == null) {
+        this.head = prev;
+        this.tail = current;
+        break;
+      }
+    }
+
+  }
+}
+
+const list = new SlinkedList<number>();
+list.insert(5);
+list.insert(6);
+list.insert(11);
+list.insert(15);
+list.insert(18);
+
+list.display();
+list.reverseInSingleLoop();
+list.display();

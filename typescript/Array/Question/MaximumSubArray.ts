ae9a8e473f3d3fd1b8e40694c0e6e66ef9eccbdf
Irfan K <unknown7k7k@gmail.com>
2024-07-02 20:35:00 +0530

    solved: array-maxsubarray

--- typescript/Array/Question/MaximumSubArray.ts
+++ typescript/Array/Question/MaximumSubArray.ts
@@ -1,19 +1,14 @@
 function maxSubArray(nums: number[]): number {
     let max = nums[0]
-    let currentSum = 0
+    let currentSum = nums[0]
 
-    for (let i = 0; i < nums.length; i++) {
-        const element = nums[i]
-        currentSum += element
-        if (element > currentSum) {
-            currentSum = element
-        }
-        if (currentSum > max)
-            max = currentSum
+    for (let i = 1; i < nums.length; i++) {
+        currentSum = Math.max(currentSum + nums[i], nums[i])
+        if(currentSum > max)max = currentSum
     }
     return max
 };
-console.log(maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]))
-console.log(maxSubArray([5, 4, -1, 7, 8]))
-console.log(maxSubArray([1]))
-console.log(maxSubArray([-1]))
+// console.log(maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]))
+// console.log(maxSubArray([5, 4, -1, 7, 8]))
+// console.log(maxSubArray([1]))
+console.log(maxSubArray([5,4,-1,7,8]))
